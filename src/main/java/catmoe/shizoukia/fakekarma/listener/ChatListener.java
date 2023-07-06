f921caa228be863e3f066540b782ea0b72396544
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-06 21:56:17 +0800

    Update ChatListener.java

--- src/main/java/catmoe/shizoukia/fakekarma/listener/ChatListener.java
+++ src/main/java/catmoe/shizoukia/fakekarma/listener/ChatListener.java
@@ -3,7 +3,8 @@ package catmoe.shizoukia.fakekarma.listener;
 import catmoe.fallencrystal.moefilter.api.event.EventListener;
 import catmoe.fallencrystal.moefilter.api.event.FilterEvent;
 import catmoe.fallencrystal.moefilter.api.event.events.bungee.AsyncChatEvent;
-import catmoe.fallencrystal.moefilter.util.message.MessageUtil;
+import catmoe.fallencrystal.moefilter.network.bungee.util.bconnection.ConnectionUtil;
+import catmoe.fallencrystal.moefilter.util.message.v2.MessageUtil;
 import catmoe.shizoukia.fakekarma.FakeKarma;
 import catmoe.shizoukia.fakekarma.config.PermissionGroup;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -14,12 +15,13 @@ public final class ChatListener implements EventListener {
     public void onChat(AsyncChatEvent event) {
         if (!event.isCancelled() && event.getMessage().equalsIgnoreCase("gg")) {
             final ProxiedPlayer player = event.getSender();
+            final ConnectionUtil connection = ConnectionUtil(player.pendingConnection);
             int point = 0;
             // 直接equals的话可能不需要!event.isBackendCommand() && !event.isProxyCommand(). 因为命令前面带"/"
 
             if (CacheCooldown.isCooldown(player)) {
                 final String cooldownMessage = FakeKarma.config.getString("cooldown-message");
-                if (!cooldownMessage.isEmpty()) { MessageUtil.INSTANCE.sendMessage(player, MessageUtil.INSTANCE.colorizeMiniMessage(cooldownMessage)); }
+                if (!cooldownMessage.isEmpty()) { MessageUtil.INSTANCE.sendMessage(connection, cooldownMessage); }
                 return;
             }
 
@@ -29,7 +31,7 @@ public final class ChatListener implements EventListener {
             }
             final String sendMessage = (FakeKarma.config.getString("message").replace("[point]", String.valueOf(point)));
             if (sendMessage.isEmpty()) { return; }
-            MessageUtil.INSTANCE.sendMessage(player, MessageUtil.INSTANCE.colorizeMiniMessage(sendMessage));
+            MessageUtil.INSTANCE.sendMessage(connection, sendMessage);
         }
     }
 }
