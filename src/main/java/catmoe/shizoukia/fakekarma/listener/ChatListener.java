e5bfd9cba44d79498956592010c7311767ae5d58
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-07 22:15:17 +0800

    Update ChatListener.java

--- src/main/java/catmoe/shizoukia/fakekarma/listener/ChatListener.java
+++ src/main/java/catmoe/shizoukia/fakekarma/listener/ChatListener.java
@@ -4,6 +4,7 @@ import catmoe.fallencrystal.moefilter.api.event.EventListener;
 import catmoe.fallencrystal.moefilter.api.event.FilterEvent;
 import catmoe.fallencrystal.moefilter.api.event.events.bungee.AsyncChatEvent;
 import catmoe.fallencrystal.moefilter.network.bungee.util.bconnection.ConnectionUtil;
+import catmoe.fallencrystal.moefilter.util.message.v2.packet.type.MessagesType
 import catmoe.fallencrystal.moefilter.util.message.v2.MessageUtil;
 import catmoe.shizoukia.fakekarma.FakeKarma;
 import catmoe.shizoukia.fakekarma.config.PermissionGroup;
@@ -21,7 +22,7 @@ public final class ChatListener implements EventListener {
 
             if (CacheCooldown.isCooldown(player)) {
                 final String cooldownMessage = FakeKarma.config.getString("cooldown-message");
-                if (!cooldownMessage.isEmpty()) { MessageUtil.INSTANCE.sendMessage(connection, cooldownMessage); }
+                if (!cooldownMessage.isEmpty()) { MessageUtil.INSTANCE.sendMessage(connection, MessagesType.CHAT, cooldownMessage); }
                 return;
             }
 
@@ -31,7 +32,7 @@ public final class ChatListener implements EventListener {
             }
             final String sendMessage = (FakeKarma.config.getString("message").replace("[point]", String.valueOf(point)));
             if (sendMessage.isEmpty()) { return; }
-            MessageUtil.INSTANCE.sendMessage(connection, sendMessage);
+            MessageUtil.INSTANCE.sendMessage(connection, MessagesType.CHAT, sendMessage);
         }
     }
 }
